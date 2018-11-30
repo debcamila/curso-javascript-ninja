a79390fd699bb0419dd400b330b551ecaa0f9a2a
Deborah Camila <camiladeborah72@gmail.com>
2018-11-30 09:56:41 -0300

    desafio eventos

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -14,3 +14,61 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+
+/* T E S T E, refazer depois */
+(function(){
+    'use strict';
+
+    var $a = document.querySelector('[data-js="link"]');
+    $a.onclick = function(event){
+        event.preventDefault();
+        alert('clicou no a de um jeito diferente');
+    };
+
+    function on(element, event, callBack){
+        document.querySelector(element)
+            .addEventListener(event, callBack, false);
+    }
+
+    function off(element, event, callBack){
+        document.querySelector(element)
+            .removeEventListener(event, callBack, false);
+    }
+
+    on('[data-js="input"]', keydown, function(event){
+        console.log(this.value);
+    });
+
+    on('[data-js="select"]', 'change', function(){
+        document.querySelector('[data-js="input"]').value = this.value;
+    });
+
+    function on(element, event, callBack){
+        document.querySelector(element)
+            .addEventListener(event, callBack, false);
+    }
+
+    on('[data-js="link"]', 'click', function(event){
+        event.preventDefault();
+        alert('clicou no a');
+    });
+
+    on('[data-js="div"]', 'click', function(){
+        alert('clicou na div');
+    });
+
+    on('[data-js="span"]', 'click', function(event){
+        alert('clicou no span');
+    });
+
+    var $a = document.querySelector('[data-js="link"]');
+    var $div = document.querySelector('[data-js="div"]')
+        .addEventListener('click', function(event){
+            alert('clicou na div');
+        }, false);
+    $a.addEventListener('click', function(event){
+        event.preventDefault();
+        alert('clicou no link');
+    }, false);
+
+});
